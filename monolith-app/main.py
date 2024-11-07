56f5623cf1eca38f5e833777612030bd4c27888d
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-07 10:47:51 +0100

    vm

--- monolith-app/main.py
+++ monolith-app/main.py
@@ -1,22 +1,13 @@
 import os
+from fastapi import FastAPI
 
 port = int(os.environ.get("PORT", default=8080))
+app = FastAPI()
 
 
-async def app(scope, receive, send):
-    assert scope["type"] == "http"
-
-    match scope["path"]:
-        case "/":
-            text, status = "Hello from vm-app\n", 200
-        case "/health":
-            text, status =  "UP\n", 200
-        case _:
-            text, status = "Page not found\n", 404
-
-    response_headers = [(b"content-type", b"text/plain; charset=utf-8")]
-    await send({"type": "http.response.start", "status": status, "headers": response_headers})
-    await send({"type": "http.response.body", "body": text.encode("UTF-8")})
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
