bd5655dcf9e4547536e758c925f86fdc4ca28161
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-07 10:27:30 +0100

    vm

--- monolith-app/main.py
+++ monolith-app/main.py
@@ -1,29 +1,24 @@
-from bottle import route, run
+import os
+
+port = int(os.environ.get("PORT", default=8080))
 
 
-@route('/')
-def index():
-    return {"message": "Hello world"}
+async def app(scope, receive, send):
+    assert scope["type"] == "http"
+
+    match scope["path"]:
+        case "/":
+            text, status = "Hello from vm-app\n", 200
+        case "/health":
+            text, status =  "UP\n", 200
+        case _:
+            text, status = "Page not found\n", 404
+
+    response_headers = [(b"content-type", b"text/plain; charset=utf-8")]
+    await send({"type": "http.response.start", "status": status, "headers": response_headers})
+    await send({"type": "http.response.body", "body": text.encode("UTF-8")})
 
 
-@route("/service-a")
-def service_a():
-    return {"message": "Hello from Service a"}
-
-
-@route("/service-b")
-def service_b():
-    return {"message": "Hello from Service b"}
-
-
-@route("/health/readiness")
-def readiness():
-    return {"message": "Ready"}
-
-
-@route("/health/liveness")
-def liveness():
-    return {"message": "Ready"}
-
-
-run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, server_header=False, access_log=False)
