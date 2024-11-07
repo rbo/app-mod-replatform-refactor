5da306e8e1b0b49a16a9bddb49adf73ef781680d
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-07 11:00:21 +0100

    monolith

--- monolith-app/main.py
+++ monolith-app/main.py
@@ -10,6 +10,21 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/service-a")
+async def service_a():
+    return {"message": "Hello from service a"}
+
+
+@app.get("/service-b")
+async def service_b():
+    return {"message": "Hello from service b"}
+
+
+@app.get("/health")
+async def health():
+    return {"status": "UP"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, server_header=False, access_log=False)
