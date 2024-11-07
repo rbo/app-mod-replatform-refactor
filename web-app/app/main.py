01e435b3f0f882cb6ea1fafc51113dd68e736221
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-07 12:56:28 +0100

    k8s files, styling

--- web-app/app/main.py
+++ web-app/app/main.py
@@ -1,37 +1,46 @@
+import os
 from flask import Flask, render_template
 import requests
 
+port = int(os.environ.get("PORT", default=3000))
+backend_host = os.environ.get("BACKEND_HOST", default="localhost")
+backend_port = int(os.environ.get("BACKEND_PORT", default=8080))
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", active="home")
 
 
 @app.route('/service-a')
 def service_a():
     try:
-        response = requests.get('http://localhost:8080/service-a')
+        response = requests.get(f'http://{backend_host}:{backend_port}/service-a')
         json_response = response.json()
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
         print("An error occurred:", e)
         json_response = {"message": "Request error"}
 
-    return render_template("service-a.html", data=json_response)
+    return render_template("service-a.html", active="service-a", data=json_response)
 
 
 @app.route('/service-b')
 def service_b():
     try:
-        response = requests.get('http://localhost:8080/service-b')
+        response = requests.get(f'http://{backend_host}:{backend_port}/service-b')
         json_response = response.json()
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
         print("An error occurred:", e)
         json_response = {"message": "Request error"}
 
-    return render_template("service-b.html", data=json_response)
+    return render_template("service-b.html", active="service-b", data=json_response)
+
+
+@app.route('/health')
+def health():
+    return {"status": "UP"}
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=3000)
+    app.run(host='0.0.0.0', debug=True, port=port)
