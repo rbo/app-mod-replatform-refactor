03e8326b5fc73fbef0691ee4916ead671258df29
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-11 13:56:12 +0100

    gunicorn conf

---
+++ web-app/gunicorn.conf.py
@@ -0,0 +1,8 @@
+
+bind = "0.0.0.0:3000"
+worker_tmp_dir = "/dev/shm"
+workers = 2
+threads = 4
+worker_class = "gthread"
+timeout = 120
+log_file = "-"
--- web-app/gunicorn_conf.py
+++
@@ -1,5 +0,0 @@
-
-bind = "0.0.0.0:3000"
-workers = 2
-threads = 2
-timeout = 120
