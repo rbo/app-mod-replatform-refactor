3bf98658446de5f83f2a4bccc05f7effc672d99d
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-11-25 19:23:46 +0100

    Containerfile, logging

--- web-app/main.py
+++ web-app/main.py
@@ -1,29 +1,31 @@
 import os
 import requests
+import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+port = int(os.environ.get("PORT", default=3000))
 backend_host = os.environ.get("BACKEND_HOST", default="localhost")
 backend_port = int(os.environ.get("BACKEND_PORT", default=8080))
 
+LOG = logging.getLogger("uvicorn.error")
 app = FastAPI(openapi_url=None)
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html", context={"active": "home"}
     )
 
 
 @app.get("/service-a", response_class=HTMLResponse)
-def service_a(request: Request):
-    json_response = call_backend("service-a")
+async def service_a(request: Request):
+    json_response = await call_backend("service-a")
 
     return templates.TemplateResponse(
         request=request, name="service-a.html", context={"active": "service-a", "data": json_response}
@@ -31,8 +33,8 @@ def service_a(request: Request):
 
 
 @app.get("/service-b", response_class=HTMLResponse)
-def service_b(request: Request):
-    json_response = call_backend("service-b")
+async def service_b(request: Request):
+    json_response = await call_backend("service-b")
 
     return templates.TemplateResponse(
         request=request, name="service-b.html", context={"active": "service-b", "data": json_response}
@@ -40,14 +42,21 @@ def service_b(request: Request):
 
 
 @app.get("/health")
-def health():
+async def health():
     return {"status": "UP"}
 
 
-def call_backend(path: str):
+async def call_backend(path: str):
     try:
         response = requests.get(f'http://{backend_host}:{backend_port}/{path}')
-        return response.json()
+        json_response = response.json()
+        LOG.info(f"Response from {backend_host}:{backend_port}/{path}: {json_response}")
+        return json_response
     except Exception as e:
-        print("An error occurred:", e)
+        LOG.error(f"Error response from {backend_host}:{backend_port}/{path}: {e}")
         return {"message": "Request error"}
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, server_header=False, access_log=False)
